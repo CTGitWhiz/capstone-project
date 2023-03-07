5f39057f11917241e5d9505649809d5427e98462
CTGitWhiz <cem276@outlook.de>
2023-03-07 14:29:49 +0100

    images loaded and dynamically displayed

---
+++ pages/components/ImageList.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+const ImageWrapper = styled.div`
+  width: 160px;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  border: 3px solid #ccc;
+`;
+
+const ImageListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 20px;
+`;
+
+const ImageList = () => {
+  const images = Array.from(
+    { length: 20 },
+    (_, i) => `/../public/Images/image${i + 1}.png`
+  );
+
+  return (
+    <ImageListWrapper>
+      {images.map((src, i) => {
+        return (
+          <ImageWrapper key={i}>
+            <Image
+              src={src}
+              alt={`My Image ${i + 1}`}
+              width={150}
+              height={150}
+              priority={i < 3}
+            />
+          </ImageWrapper>
+        );
+      })}
+    </ImageListWrapper>
+  );
+};
+
+export default ImageList;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import styled from "styled-components";
+import ImageList from "./components/ImageList";
+
+const Container = styled.div`
+  max-width: 100%;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+`;
 
 export default function Home() {
   return (
-    <main>
-      <Heading>🐋Capstone Whales Template🐋</Heading>
-    </main>
+    <Container>
+      <Title>Welcome to my Next.js app!</Title>
+      <ImageList />
+    </Container>
   );
 }
-
-const Heading = styled.h1`
-text-align: center;
-`;
