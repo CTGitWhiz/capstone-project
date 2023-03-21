8f9a5c91a420e4c6b88917eecb647239c6bcb4d9
CTGitWhiz <cem276@outlook.de>
2023-03-22 00:15:21 +0100

    Update image selection logic to consider latest timestamp with same or higher ratings

--- pages/supreme.js
+++ pages/supreme.js
@@ -48,40 +48,51 @@ const categories = [
   "Realism",
 ];
 
+// Function to check if the code is running in a client-side environment and get the range values
 function getClientSideRangeValues() {
+  // If the window object is undefined, return null
   if (typeof window === "undefined") {
     return null;
   }
+  // If the window object is defined, return the stringified range values from local storage
   return JSON.stringify(localStorage.getItem("rangeValues"));
 }
 
 export default function Supreme() {
   const [topRatedImages, setTopRatedImages] = useState([]);
 
+  // useEffect hook to load top-rated images when the component is mounted or range values change
   useEffect(() => {
+    // If getClientSideRangeValues() returns a falsy value, do not run the effect
     if (!getClientSideRangeValues()) {
       return;
     }
 
+    // Get saved range values from local storage, or use an empty object if they don't exist
     const savedRangeValues =
       JSON.parse(localStorage.getItem("rangeValues")) || {};
 
+    // If there are no saved range values, do not run the effect
     if (Object.keys(savedRangeValues).length === 0) {
       return;
     }
 
+    // Find the highest-rated images for each category
     const highestRatedImages = categories.map((_, index) => {
       let highestRatedImageId = null;
-      let highestRating = -1;
+      let highestRating = 0; // Change the initial value of highestRating to 0
       let latestTimestamp = 0;
 
+      // Iterate over the saved range values and find the highest-rated image
       for (const imageId in savedRangeValues) {
         const rating = parseInt(savedRangeValues[imageId][index]);
         const timestamp = savedRangeValues[imageId][5];
 
+        // Update the highest-rated image if a higher rating (greater than 0) is found,
+        // or if the rating is the same or higher, always consider the more recent timestamp
         if (
-          rating > highestRating ||
-          (rating === highestRating && timestamp > latestTimestamp)
+          (rating > highestRating && rating > 0) ||
+          (rating >= highestRating && timestamp > latestTimestamp && rating > 0)
         ) {
           highestRating = rating;
           highestRatedImageId = imageId;
@@ -92,10 +103,12 @@ export default function Supreme() {
       return { categoryIndex: index, imageId: highestRatedImageId };
     });
 
+    // Update the topRatedImages state with the highest-rated images
     setTopRatedImages(highestRatedImages);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getClientSideRangeValues()]);
 
+  // Render the top-rated images showcase
   return (
     <SupremeContainer>
       <SupremeTitle>Top-Rated Images Showcase</SupremeTitle>
