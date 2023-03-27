ec32a52f4598c09213ad574e189236b6149a2422
CTGitWhiz <cem276@outlook.de>
2023-03-27 16:56:21 +0200

    fix import styling - @

--- pages/supreme.js
+++ pages/supreme.js
@@ -7,7 +7,7 @@ import {
   CategoryTitle,
   ImageContainer,
   NoImageMessage,
-} from "../styles/Supreme.styles.js";
+} from "@/styles/Supreme.styles.js";
 
 // Categories array
 const categories = [
